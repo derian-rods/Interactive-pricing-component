af5cf9b8ade63adf26c14597f1f30b733791a4ef
Derian Rodriguez Salazar <derian.rods@gmail.com>
2021-04-05 14:57:00 +0200

    🚀 end of challenge

---
+++ js/app.js
@@ -0,0 +1,31 @@
+/* Ref HTMl */
+
+const pages = document.querySelector('.card__header-text');
+const price = document.querySelector('.price');
+const range = document.querySelector('#range');
+const progressBar = document.querySelector('.progressBar');
+
+const calculePrice = (value) => {
+    return new Promise((resolve, reject)=>{
+        const basePage = value *2;
+        const basePrice= (value * 0.32).toFixed(2);
+       (value && value <= 100) ? 
+       resolve({
+            pagesView: basePage,
+            price: basePrice
+        }) :
+        reject("Fail in calculation of price");
+    })
+
+}
+
+range.addEventListener('change', async ({target}) => {
+    const pageAndPrice = await calculePrice(target.value);
+    pages.innerHTML= `<span class="card__header-text">${pageAndPrice.pagesView}k Pageviews</span>`
+    price.innerHTML= `$ ${pageAndPrice.price}`;
+    progressBar.style.width = target.value + '%';
+})
+
+// calculePrice(100)
+// .then(res => console.log(res))
+// .catch(err=> console.log(err))
